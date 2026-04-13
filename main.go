4c68386328807f0df5a504e4419b7c66824a2045
Lucas Schweickert <cyberfly@gmx.net>
2026-04-13 16:47:24 +0200

    simplified cleanInput, added REPL

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+		words := cleanInput(input)
+		fmt.Printf("Your command was: %v\n", words[0])
+	}
 }
--- repl.go
+++ repl.go
@@ -1,23 +1,11 @@
 package main
 
+import (
+	"strings"
+)
+
 func cleanInput(text string) []string {
-	result := make([]string, 0)
-	currentWord := ""
-	for _, char := range text {
-		if char > 32 && char < 127 {
-			if char >= 'A' && char <= 'Z' {
-				char += 32 // Convert to lowercase
-			}
-			currentWord += string(char)
-		} else {
-			if currentWord != "" {
-				result = append(result, currentWord)
-				currentWord = ""
-			}
-		}
-	}
-	if currentWord != "" {
-		result = append(result, currentWord)
-	}
+	lower := strings.ToLower(text)
+	result := strings.Fields(lower)
 	return result
 }
